fa21f3123c626a557c2722dd919011a02f226504
delberss <delberss@hotmail.com>
2023-05-05 16:51:33 -0300

    Aplicando Styled components no lugar do css separado

--- src/componentes/Logo/index.jsx
+++ src/componentes/Logo/index.jsx
@@ -1,16 +1,23 @@
 import logo from '../../imagens/logo.svg';
-import './style.css'
+import styled from 'styled-components';
 
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const ImagemLogo = styled.img`
+    margin-right: 10px;
+`
 function Logo(){
     return(
-        <div className='logo'>
-            <img 
+        <LogoContainer>
+            <ImagemLogo 
                 src={logo} 
                 alt='logo'
-                className='logo-img'    
             />
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
--- src/componentes/OpcoesHeader/index.jsx
+++ src/componentes/OpcoesHeader/index.jsx
@@ -1,16 +1,33 @@
-import './style.css'
+import styled from 'styled-components';
+
+const Opcoes = styled.ul`
+    display: flex;
+`;
+const Opcao = styled.li`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 16px;
+    height: 100%;
+    min-width: 120px;
+    padding: 0 5px;
+
+    cursor: pointer;
+`;
 
 const textoOpcoes = ['CATEGORIAS','FAVORITOS','MINHA ESTANTE'];
 
+
 function OpcoesHeader(){
     return(
-        <ul className='opcoes'>
+        <Opcoes>
             {
                 textoOpcoes.map((item) => (
-                <li className='opcao'><p>{item}</p></li>
+                <Opcao><p>{item}</p></Opcao>
                 ))
             }
-        </ul>
+        </Opcoes>
     )
 }
 
