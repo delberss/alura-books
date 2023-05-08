2db0dddafbe111662a793a76f7c18be0b91ad048
delberss <delberss@hotmail.com>
2023-05-08 12:03:22 -0300

    Lista de livros

---
+++ src/componentes/Lancamentos/dadosLancamentos.js
@@ -0,0 +1,15 @@
+import livro1 from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+
+export const livrosLancamentos = [
+    {
+        nome: 'Liderança em Design',
+        id: 1,
+        src: livro1
+    },
+    {
+        nome: 'JavaScript Assertivo',
+        id: 2,
+        src: livro2
+    },
+]
---
+++ src/componentes/Search/dadosSearch.js
@@ -0,0 +1,25 @@
+import livro1 from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+
+export const livros = [
+    {
+        nome: 'Liderança em Design',
+        id: 1,
+        src: livro1
+    },
+    {
+        nome: 'JavaScript Assertivo',
+        id: 2,
+        src: livro2
+    },
+    {
+        nome: 'Cangaceiro em JavaScript',
+        id: 3,
+        src: livro1
+    },
+    {
+        nome: 'Aventureiros do Java',
+        id: 4,
+        src: livro2
+    }
+]
